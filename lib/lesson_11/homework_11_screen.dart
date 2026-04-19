caed67773c0c43611550018f2437a3e7206be415
Vitaliy <zhilovskij1986@gmail.com>
2026-04-19 16:23:48 +0300

    Subtask 4 — Різні варіанти вирівнювання

--- lib/lesson_11/homework_11_screen.dart
+++ lib/lesson_11/homework_11_screen.dart
@@ -10,33 +10,35 @@ class Homework11Screen extends StatelessWidget {
         child: Column( 
           mainAxisAlignment: MainAxisAlignment.center,                   
           children: [
-            Container(
-              width: 300,
-              height: 150,
-              margin: const EdgeInsets.only(top: 8.0),
-              decoration: const BoxDecoration(
-                color: Colors.blue,
-                borderRadius: BorderRadius.all(Radius.circular(16.0)),
-              ),
-              alignment: Alignment.bottomRight,
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const Icon(Icons.star, color: Colors.amber, size: 24),
-                    const SizedBox(width: 10),
-                    const Text(
-                      'Привіт, Flutter!',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
+            Expanded(
+              child: Container(
+                width: 300,
+                height: 150,
+                margin: const EdgeInsets.only(top: 8.0),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                ),
+                alignment: Alignment.topCenter,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      const Icon(Icons.star, color: Colors.amber, size: 24),
+                      const SizedBox(width: 10),
+                      const Text(
+                        'Привіт, Flutter!',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                    const SizedBox(width: 10),
-                    const Icon(Icons.star, color: Colors.amber, size: 24),
-                  ],
+                      const SizedBox(width: 10),
+                      const Icon(Icons.star, color: Colors.amber, size: 24),
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -44,11 +46,11 @@ class Homework11Screen extends StatelessWidget {
               width: 300,
               height: 150,
               margin: const EdgeInsets.only(top: 8.0),
-              decoration: const BoxDecoration(
+              decoration: BoxDecoration(
                 color: Colors.green,
                 borderRadius: BorderRadius.all(Radius.circular(16.0)),
               ),
-              alignment: Alignment.bottomRight,
+              alignment: Alignment.center,
               child: Padding(
                 padding: const EdgeInsets.all(10.0),
                 child: Row(
@@ -74,7 +76,7 @@ class Homework11Screen extends StatelessWidget {
               width: 300,
               height: 150,
               margin: const EdgeInsets.only(top: 8.0),
-              decoration: const BoxDecoration(
+              decoration: BoxDecoration(
                 color: Colors.red,
                 borderRadius: BorderRadius.all(Radius.circular(16.0)),
               ),
