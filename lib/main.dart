fffc7e34244b4895774ce7e84622ba3fc9f4abe6
Vitaliy <zhilovskij1986@gmail.com>
2026-05-02 16:56:43 +0300

    feat: add lesson 13 navigation with FadeTransition

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter_homework/lesson_11/homework_11_screen.dart';
 import 'package:flutter_homework/lesson_12/homework_12_screen.dart';
 import 'package:flutter_homework/lesson_13/homework_13_screen.dart';
 
-
 void main() {
   runApp(const FlutterWidgetsApp());
 }
@@ -13,9 +12,7 @@ class FlutterWidgetsApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeScreen(),
-    );
+    return const MaterialApp(home: HomeScreen());
   }
 }
 
@@ -29,51 +26,113 @@ class HomeScreen extends StatelessWidget {
         title: const Text('Flutter Lab'),
         backgroundColor: Colors.blue.shade100,
       ),
-      body: SingleChildScrollView(
+      body: ListView(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            FeatureCard(
-              title: 'lesson_11',
+        children: [
+          Card(
+            shape: RoundedRectangleBorder(
+              side: BorderSide(color: Colors.blue, width: 1),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: ListTile(
+              leading: const Icon(Icons.block),
+              title: Text('lesson_11'),
+              trailing: Icon(Icons.arrow_forward_ios),
               onTap: () => Navigator.push(
                 context,
-                MaterialPageRoute<Widget>(
-                  builder: (context) => const Homework11Screen(),
+                PageRouteBuilder<Widget>(
+                  transitionDuration: const Duration(milliseconds: 800),
+                  pageBuilder: (context, animmation, secondaryAnimation) =>
+                      const Homework11Screen(),
+                  transitionsBuilder:
+                      (context, animmation, secondaryAnimation, child) =>
+                          SlideTransition(
+                            position: Tween<Offset>(
+                              // begin: const Offset(1, 0),
+                              //end: Offset.zero,
+                              begin: const Offset(0, 1),
+                              end: Offset.zero,
+                            ).animate(animmation),
+                            child: child,
+                          ),
                 ),
               ),
             ),
-             FeatureCard(
-              title: 'lesson_12',
+          ),
+          Card(
+            shape: RoundedRectangleBorder(
+              side: BorderSide(color: Colors.blue, width: 1),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: ListTile(
+              leading: const Icon(Icons.pages),
+              title: Text('lesson_12'),
+              trailing: Icon(Icons.arrow_forward_ios),
               onTap: () => Navigator.push(
                 context,
-                MaterialPageRoute<Widget>(
-                  builder: (context) => const Homework12Screen(),
+                PageRouteBuilder<Widget>(
+                  transitionDuration: const Duration(milliseconds: 800),
+                  pageBuilder: (context, animmation, secondaryAnimation) =>
+                      const Homework12Screen(),
+                  transitionsBuilder:
+                      (context, animmation, secondaryAnimation, child) =>
+                          RotationTransition(turns: animmation, child: child),
                 ),
               ),
             ),
-              FeatureCard(
-              title: 'lesson_12',
+          ),
+          Card(
+            shape: RoundedRectangleBorder(
+              side: BorderSide(color: Colors.blue, width: 1),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: ListTile(
+              leading: const Icon(Icons.task),
+              title: Text('lesson_13'),
+              trailing: Icon(Icons.arrow_forward_ios),
               onTap: () => Navigator.push(
                 context,
-                MaterialPageRoute<Widget>(
-                  builder: (context) => const WidgetConstrainsTrainingScreen(),
+                PageRouteBuilder<Widget>(
+                  transitionDuration: const Duration(milliseconds: 800),
+                  pageBuilder: (context, animmation, secondaryAnimation) =>
+                      const WidgetConstrainsTrainingScreen(),
+                  transitionsBuilder:
+                      (context, animmation, secondaryAnimation, child) =>
+                          FadeTransition(opacity: animmation, child: child),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
 }
 
+//  FeatureCard(
+//   title: 'lesson_12',
+//   onTap: () => Navigator.push(
+//     context,
+//     MaterialPageRoute<Widget>(
+//       builder: (context) => const Homework12Screen(),
+//     ),
+//   ),
+// ),
+//   FeatureCard(
+//   title: 'lesson_13',
+//   onTap: () => Navigator.push(
+//     context,
+//     MaterialPageRoute<Widget>(
+//       builder: (context) => const WidgetConstrainsTrainingScreen(),
+//     ),
+//   ),
+// ),
+//     );
+//   }
+// }
+
 class FeatureCard extends StatelessWidget {
-  const FeatureCard({
-    required this.title,
-    required this.onTap,
-    super.key,
-  });
+  const FeatureCard({required this.title, required this.onTap, super.key});
 
   final String title;
   final VoidCallback onTap;
@@ -84,7 +143,7 @@ class FeatureCard extends StatelessWidget {
       elevation: 2,
       margin: const EdgeInsets.only(bottom: 15.0),
       child: InkWell(
-        onTap: onTap,        
+        onTap: onTap,
         highlightColor: Colors.blue.shade100,
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
